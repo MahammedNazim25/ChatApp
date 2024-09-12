17232cc6467c77619784faf8d8b0ae0d476e677e
Rachna2030 <rachnaaulakh@GMAIL.COM>
2024-09-11 22:52:07 -0400

    help commit

---
+++ Help.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Help() {
+    return (
+       
+            <div className="contactus-container">
+              <header>
+                <nav>
+                   <a href="/Policy">Policy</a> 
+                  <a href="/contactus">Help</a>
+                  <a href="/contactus">Contact Us</a>
+                  
+                </nav>
+              </header>
+              </div>
+    );
+}
+export default Help;
